081541fea1e202110636d532bb736885866c3306
UgoBena <ugo.benassayag@gmail.com>
2020-06-23 14:20:22 +0200

    added all basic functionnalities and tested them locally. To be pushed to heroku

--- Backend/models/Recipe.js
+++ Backend/models/Recipe.js
@@ -1,21 +1,56 @@
 const mongoose = require("mongoose");
 
 const RecipeSchema = mongoose.Schema({
-  name:String,
+  name:{
+    type: String,
+    required: true,
+    unique:true,
+  },
   overview:String,
   prep_time:String,
   cook_time:String,
-  dish_type:String,
+  dish_type:{
+    type: String,
+    required: true
+  },
   categories:[String],
-  ingredients:[{
-    quantity:Number,
-    unit:String,
-    name:String
-  }],
-  steps:[{
-    description:String,
-    image:String
-  }],
+  ingredients:{
+    type:[{
+      quantity:{
+        type: Number,
+        required: true
+      },
+      unit:{
+        type: String,
+        required: true
+      },
+      name:{
+        type: String,
+        required: true
+      },
+    }],
+    validate: {
+      validator: function(v) {
+        return this.ingredients.length>0;
+      },
+      message: props => "You must enter at least one ingredient"
+    }
+  },
+  steps:{
+    type:[{
+      description:{
+        type: String,
+        required: true
+      },
+      image:String
+    }],
+    validate: {
+      validator: function(v) {
+        return this.steps.length>0;
+      },
+      message: props => "You must enter at least one step"
+    },
+  },
   comments:[String]
 })
 
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -8,7 +8,7 @@ const test = new Recipe({
   overview: "on va se régaleeer",
   prep_time: "20min",
   cook_time:"30min",
-  dish_type:"starter",
+  dish_type:"maindish",
   categories:["méditerranéen","asiatique"],
   ingredients:[{
     quantity:40,
@@ -31,10 +31,78 @@ const test = new Recipe({
   ],
   comments:["vraiment on se régale", "NRV"]
 });
+test.save();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send(test);
+/* GET ALL recipes. */
+router.get('/getRecipes', function(req, res, next) {
+  Recipe.find(function(err,recipe){
+    if (err) res.send(err);
+    res.send(recipe);
+  })
 });
+
+/* Filter recipes by dish_type */
+router.get('/getRecipesByDishType', function(req, res, next) {
+  const filter = {dish_type:req.query.dishType};
+  Recipe.find(filter,function(err,recipe){
+    if (err) res.send(err);
+    res.send(recipe);
+  })
+});
+
+/* Filter recipe by category */
+router.post('/getRecipesBycategories', function(req, res, next) {
+  const filter = {categories:req.body.categories};
+  Recipe.find(filter,function(err,recipe){
+    if (err) res.send(err);
+    res.send(recipe);
+  })
+});
+
+/* Get specific recipe by name (unique) */
+router.get('/getRecipeByName', function(req, res, next) {
+  const filter = {dish_type:req.query.name};
+  Recipe.find(filter,function(err,recipe){
+    if (err) res.send(err);
+    res.send(recipe);
+  })
+});
+
+/* Get recipies by regexp */
+//TODO
+
+/* Add a new recipe */
+router.post('/addRecipe',function(req,res,next){
+  var data = req.body;
+  const newRecipe = new Recipe({
+    name: data.name,
+    overview: data.overview,
+    prep_time: data.prep_time,
+    cook_time:data.cook_time,
+    dish_type:data.dish_type,
+    categories:data.categories,
+    ingredients:data.ingredients,
+    steps:data.steps,
+    comments:data.comments
+  })
+  newRecipe.save(function(err,recipe){
+    if (err) res.send(err);
+    res.send(recipe);
+  });
+})
+
+/* Add comment to a recipe */
+router.post('/addComment',function(req,res,next){
+  var data = req.body;
+  if (typeof(data.recipeName) !== "string") res.status(400).send("You must provide a recipeName");
+  if (!data.comment || data.comment.length === 0) res.status(400).send("You must provide a comment");
+  Recipe.findOne({name:data.recipeName},function(err,recipe){
+    if (err) res.send(err);
+    Recipe.update({name:data.recipeName},{comments:[...recipe.comments,data.comment]});
+    res.send("comment added");
+  })
+})
+
+
 
 module.exports = router;
